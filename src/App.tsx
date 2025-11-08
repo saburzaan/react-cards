2ea272c93c6478d528819bbdfe6f3f148cb5437e
Zhaslanuly Sabyrzhan <saburzaan@gmail.com>
2025-11-08 23:41:28 +0500

    feat: Not Found Page

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import MainLayout from './components/MainLayout'
 import HomePage from './pages/HomePage'
+import NotFoundPage from './pages/NotFoundPage'
 
 function App() {
 
@@ -11,6 +12,7 @@ function App() {
           <Route path="/" element={<HomePage />} />
           <Route path="/add-question" element={<div>add question</div>} />
           <Route path="/question/:id" element={<div>question page</div>} />
+          <Route path="*" element={<NotFoundPage />} />
         </Route>
       </Routes>
     </BrowserRouter>
