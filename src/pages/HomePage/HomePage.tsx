8a0cebd43d21ca9b88261da0be174b20f49bdb82
Zhaslanuly Sabyrzhan <saburzaan@gmail.com>
2025-11-09 13:43:29 +0500

    feat: delayFn

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -3,19 +3,24 @@ import { API_URL } from '../../constants'
 import { useEffect, useState } from 'react'
 import QuestionCardList from '../../components/QuestionCardList'
 import Loader from '../../components/Loader'
+import { delayFn } from '../../helpers/delayFn.ts'
 
 const HomePage = () => {
-
   const [questions, setQuestions] = useState([])
+  const [isLoading, setIsLoading] = useState(false)
 
   const getQuestions = async () => {
     try {
+      setIsLoading(true)
+      await delayFn()
       const response = await fetch(`${API_URL}/react`)
       const questions = await response.json()
       setQuestions(questions)
 
     } catch (error) {
       console.error('Failed to load questions:', error)
+    } finally {
+      setIsLoading(false)
     }
   }
 
@@ -25,7 +30,7 @@ const HomePage = () => {
 
   return (
     <>
-      <Loader />
+      {isLoading && <Loader />}
       <QuestionCardList cards={questions} />
     </>
   )
