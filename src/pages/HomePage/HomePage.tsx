1226bd8a6d54cf7e5cde8cdca322f7189c5fac3d
Zhaslanuly Sabyrzhan <saburzaan@gmail.com>
2025-11-09 12:45:09 +0500

    feat: get data from server, useEffect,

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,75 +1,41 @@
 import './HomePage.css'
 import QuestionCard from '../../components/QuestionCard'
+import { API_URL } from '../../constants'
+import { useEffect, useState } from 'react'
 
-const cards = [
-  {
-    'id': '1',
-    'question': 'Что такое React?',
-    'answer': 'React — это библиотека для создания пользовательских интерфейсов.',
-    'description': 'React — это JavaScript-библиотека, разработанная Facebook, которая используется для построения UI с компонентным подходом. React позволяет вам создавать пользовательские интерфейсы из отдельных частей, называемых компонентами.',
-    'resources': [
-      'https://react.dev',
-      'https://react.dev/reference/react',
-    ],
-    'level': 1,
-    'completed': true,
-    'editDate': '03.02.2025, 19:49',
-  },
-  {
-    'id': '2',
-    'question': 'Что такое JSX?',
-    'answer': 'JSX — это синтаксическое расширение JavaScript для React.',
-    'description': 'JSX позволяет писать HTML-подобный код в JavaScript, который затем транспилируется в вызовы `React.createElement`. Он облегчает создание и визуальное представление структуры компонентов.',
-    'resources': [
-      'https://react.dev/learn/writing-markup-with-jsx',
-    ],
-    'level': 2,
-    'completed': false,
-    'editDate': '03.02.2025, 20:25',
-  },
-  {
-    'id': '3',
-    'question': 'Какой основной принцип работы React?',
-    'answer': 'React использует Virtual DOM для оптимизации рендеринга.',
-    'description': 'React поддерживает концепцию Virtual DOM — это представление реального DOM в памяти. Когда состояние компонента изменяется, React сравнивает Virtual DOM с предыдущим состоянием и обновляет только измененные элементы на странице.',
-    'resources': [
-      'https://react.dev/learn/render-and-commit',
-    ],
-    'level': 2,
-    'completed': false,
-    'editDate': '03.02.2025, 19:01',
-  },
-  {
-    'id': '4',
-    'question': 'Как создать компонент в React?🍩',
-    'answer': 'Компонент можно создать как функцию или класс.',
-    'description': 'Функциональные компоненты — это функции, принимающие `props` и возвращающие JSX. Классовые компоненты — это классы, наследуемые от `React.Component`, с методом `render`.',
-    'resources': [
-      'https://react.dev/learn/your-first-component',
-    ],
-    'level': 1,
-    'completed': false,
-    'editDate': '21.02.2025, 20:26',
-  },
-  {
-    'id': '5',
-    'question': 'Что такое props в React?',
-    'answer': 'Props — это входные параметры компонентов.',
-    'description': 'Props (свойства) используются для передачи данных от родительского компонента к дочернему. Они неизменяемы внутри компонента и позволяют делать компоненты более гибкими и переиспользуемыми.',
-    'resources': [
-      'https://react.dev/learn/passing-props-to-a-component',
-    ],
-    'level': 1,
-    'completed': false,
-  },
-]
+interface Question {
+  id: string
+  level: number
+  completed: boolean
+  question: string
+  answer: string
+}
 
 const HomePage = () => {
 
+  const [questions, setQuestions] = useState<Question[]>([])
+
+  const getQuestions = async () => {
+    try {
+      const response = await fetch(`${API_URL}/react`)
+      const questions = await response.json()
+      setQuestions(questions)
+
+    } catch (error) {
+      console.error('Failed to load questions:', error)
+    }
+  }
+
+  useEffect(() => {
+    getQuestions()
+  }, [])
+
   return (
     <main className="home-page">
-      {cards.map((card) => (
-        <QuestionCard key={card.id} id={card.id} level={card.level} completed={card.completed} question={card.question} answer={card.answer} />
+      {questions.map((q) => (
+        <QuestionCard key={q.id} id={q.id} level={q.level} completed={q.completed}
+                      question={q.question}
+                      answer={q.answer} />
       ))}
     </main>
   )
